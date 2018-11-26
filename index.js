2c75768c4998fb12c8ce29ca0c2f2dc95cbf914e
Daeun Kim <eileenkim1208@gmail.com>
2018-11-26 14:12:30 +0900

    Get google info

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const config = require('config');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy(
+  {
+    clientID: config.auth.google.clientID,
+    clientSecret: config.auth.google.clientSecret,
+    callbackURL: "/auth/google/callback"
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log(`access-token => ${accessToken}`);
+    console.log(`refresh-token => ${refreshToken}`);
+    console.log(`profile => `,profile);
+    console.log(`done => ${done}`);  // done = fucn
+  }
+));
 
-app.get('/',(req,res)=>{
-  res.send({happy: 'hacking'});
-})
+app.get('/', (req, res) => {
+  res.send({ happy: 'hacking' });
+});
+
+app.get(
+  '/auth/google',  // user req toss
+  passport.authenticate('google', { scope: ['profile','email'] })  // middelware func
+);
+
+app.get(
+  '/auth/google/callback',  //req + code => google => Real user data
+  passport.authenticate('google') // can not get 
+);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=> console.log())
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
