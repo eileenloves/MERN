0d9f6ae1f6420139271098f14420c26d25d952e1
Daeun Kim <eileenkim1208@gmail.com>
2018-11-26 10:58:02 +0900

    Init repo

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+app.get('/',(req,res)=>{
+  res.send({happy: 'hacking'});
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=> console.log())
